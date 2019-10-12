d0bd374cdbf526dc89f814b428f036a66851c974
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-10-12 18:06:20 +0900

    aho

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) =>  {
     res.send('hello');
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT | 3000, () => {
+app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 });
